376d0a5885edaf75d96aa70363229855b7eb2adf
Bogdan Radu <radumugur1997@gmail.com>
2023-10-27 02:02:14 +0300

    small update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
-console.log("muie la cai");
-console.log("sdads")
+const express = require('express');
+const app = express();
+
+app.use(express.raw({limit:800000}));
+
+
+// function firstMiddleware(req,res,next){
+//     console.log(req.get('Content-Type'));
+//     console.log(req.method);
+//     console.log("first middleware called");
+//     res.myData="This is my date";
+//     next();
+// }
+
+// function secondMiddleware(req,res,next){
+//     console.log("second middleware called");
+//     console.log(res.myData);
+//     next();
+// }
+
+// app.use([firstMiddleware,secondMiddleware]);
+
+const myObject={
+    dex:'asd',
+    info:'dex'
+};
+
+app.get("/api/getInfo",(req,res)=>{
+    res.json(myObject);
+});
+app.get("/api/sendInfo",express.json(),(req,res)=>{
+    console.log(req.get('Content-Type'));
+    console.log(typeof req.body);
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.listen("6632");
